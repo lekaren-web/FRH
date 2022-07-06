97da423c5f4f8c5d41685acb845f7e2d8b5ee079
lekaren-web <67718423+lekaren-web@users.noreply.github.com>
2022-07-06 17:49:32 -0400

    rebuild

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ CORS(app)
 @cross_origin()
 def my_profile():
     response_body = {
-        "name": "Nagato",
+        "name": "Karen Le",
         "about": "Hello! I'm a full stack developer that loves python and javascript",
         "image": "https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80"
     }
